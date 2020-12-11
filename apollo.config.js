14dc71d419beb6e8a4994ab9856b41327d0353dd
Danis Gumerov <danis_gumerov@epam.com>
2020-12-11 11:26:23 +0500

    feat apollo config
    --no-verify

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     client: {
-        includes: ['./components/*.tsx'],
+        includes: ['./components/**/*.tsx', './pages/**/*.tsx'],
         service: {
             name: 'AnilistAPI',
             localSchemaFile: './schema.graphql'
