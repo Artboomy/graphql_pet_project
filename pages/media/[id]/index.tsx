659508d97d07eb264f6d9082bf9678c301f017e4
Danis Gumerov <danis_gumerov@epam.com>
2020-12-11 12:37:51 +0500

    fix wrong id blink

--- pages/media/[id]/index.tsx
+++ pages/media/[id]/index.tsx
@@ -1,11 +1,19 @@
 import { useRouter } from 'next/router';
 import * as React from 'react';
 import Wrapper from '../../../components/wrapper';
-import { Box, Link, Typography } from '@material-ui/core';
+import {
+    Backdrop,
+    Box,
+    CircularProgress,
+    createMuiTheme,
+    Link
+} from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import { Fab } from '@material-ui/core';
 import MediaView from '../../../components/media';
 import styles from './index.module.scss';
+
+const theme = createMuiTheme();
 
 const MediaPage = (): JSX.Element => {
     const router = useRouter();
@@ -16,7 +24,13 @@ const MediaPage = (): JSX.Element => {
                 {typeof id === 'string' ? (
                     <MediaView id={id} />
                 ) : (
-                    <Typography>Wrong Id</Typography>
+                    <Backdrop
+                        open={true}
+                        style={{
+                            backgroundColor: theme.palette.background.default
+                        }}>
+                        <CircularProgress color={'secondary'} />
+                    </Backdrop>
                 )}
                 <Box className={styles.fabButton}>
                     <Link href={'/'}>
